95f4c81b94be1fc7f8709f1848b7c2f698ec9928
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-02-10 22:18:09 -0500

    Version en producción

---
+++ build/precache-manifest.f8984d083d6deef78eb48c5d2c1cdab4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8607a9dc90c618f2347e0ece47e4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0839e45aa44ecac3a9e",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4572ba007aebfd3feac9",
+    "url": "/static/css/main.ae4d0ccb.chunk.css"
+  },
+  {
+    "revision": "a0839e45aa44ecac3a9e",
+    "url": "/static/js/2.6e2f5ea6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6e2f5ea6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4572ba007aebfd3feac9",
+    "url": "/static/js/main.03aa8cc4.chunk.js"
+  },
+  {
+    "revision": "24dc0ae7432932722a76",
+    "url": "/static/js/runtime-main.fa3dd7ff.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/static/media/banner.42d6efdb.png"
+  },
+  {
+    "revision": "960a1abf7958f13d2617fba06264ae60",
+    "url": "/static/media/loader.960a1abf.gif"
+  }
+]);
